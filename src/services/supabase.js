507365e05b30e17db0ad0382f2746b661785e5a2
braziel <braziel@discente.ufg.br>
2025-06-21 10:10:34 -0300

    Fixing Issues

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://itmirtvfihjrabvqrhmt.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0bWlydHZmaWhqcmFidnFyaG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NDEwNDcsImV4cCI6MjA2NDExNzA0N30.vYJ_xI1Ezgv4Rv4C8g9SBVndDypO5Y9eBzc7bsIyAwA";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
